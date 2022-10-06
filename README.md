a3fe124de43ba32820578451c661cc197c1686b4
Guled Ibrahim <guled@eddlondon.com>
2022-10-06 10:30:10 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# EDD Eslint Config  
+
+Our default export contains ESlint rules, including ECMAScript 6+ and React.
+
+## Prerequisites
+`npm install npm@latest -g`
+
+`npm install --global yarn`
+
+`eslint extension`
+
+## Installation
+`npm i @EDD/eslint-config-EDD`
+`yarn add @EDD/eslint-config-EDD`
+
+## Usage
++ create a file called `.eslintrc.json` in root directory of application 
+
++ paste the following snippet <code>{<br/> &nbsp;&nbsp;extends: ['EDD'];<br/>}</code>. 
+  
---
+++ index.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "next/core-web-vitals",
+    "plugin:jsx-a11y/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["jsx-a11y"],
+  rules: {
+    "unicorn/no-null": 0,
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always-and-inside-groups",
+      },
+    ],
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          kebabCase: true,
+          pascalCase: true,
+        },
+        ignore: ["setupTests.ts", "reportWebVitals.ts"],
+      },
+    ],
+    "array-callback-return": [
+      "error",
+      {
+        allowImplicit: true,
+      },
+    ],
+    "unicorn/catch-error-name": [
+      "error",
+      {
+        name: "exception",
+      },
+    ],
+    "unicorn/no-useless-undefined": ["error", { checkArguments: false }],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
