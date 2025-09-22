363bf821ec9e2a5ca85567f612c7cf61dbba7018
Henry Hobhouse <henry@hob.house>
2025-09-22 08:09:13 +0100

    chore: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+module.exports = {
+  // Override defaults
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+};
